fbf4f2e1f717558c4286d24e7166873c2d3bfc75
Kevin Chi <kevinchi118@gmail.com>
2019-12-26 17:51:22 -0500

    updates packages, allows split only on same value starting hand

--- src/game/index.js
+++ src/game/index.js
@@ -13,6 +13,7 @@ import {calculateWinner} from '../actions/calculateWinner';
 import {changeBet} from '../actions/changeBet';
 import {double} from '../actions/double';
 import {split} from '../actions/split';
+import {cardMap} from '../helpers/cardMap';
 import {calculateHandTotal} from '../helpers/calculateHandTotal';
 import cardLogo from '../images/card_logo.png';
 
@@ -76,6 +77,15 @@ class Game extends React.Component {
     }
   }
 
+  isSplitableHand = () => {
+    if (this.props.player.isSplit) {
+      return false
+    } 
+    const conditionOne = this.props.player.handTotal < 21
+    const conditionTwo = cardMap[this.props.player.hand[0].value] === cardMap[this.props.player.hand[1].value]
+    return conditionOne && conditionTwo
+  }
+
   renderActions = () => {
     const player = this.props.player;
     if (player.status === 'betting') {
@@ -105,7 +115,7 @@ class Game extends React.Component {
       } else {
         return (
         <React.Fragment>
-          {player.handTotal < 21 && !player.isSplit ? <button onClick={this.props.split}>Split</button> : null}
+          {this.isSplitableHand() ? <button onClick={this.props.split}>Split</button> : null}
           {player.handTotal < 21 && !player.isSplit ? <button onClick={this.double}>Double</button> : null}
           {player.handTotal < 21 ? <button onClick={this.hit}>Hit</button> : null}
           <button onClick={this.stand}>Stand</button>
@@ -129,12 +139,15 @@ class Game extends React.Component {
       return (    
         <div className='rules'>
           <button onClick={this.toggleShowRules}>Toggle Rules</button>
-          <p>Welcome to my version of BlackJack! Here are some house rules:</p>
-          <p>Hitting 21 is not a gauranteed win against dealer and does not pay 3 to 2</p>
-          <p>Player may double on any normal hand</p>
-          <p>Player may split on any hand</p>
+          <p>Welcome to my version of Blackjack!</p>
+          <p>If you are not familiar with the game, you can find general rules <a href='http://www.hitorstand.net/strategy.php' target="_blank">here</a></p>
+          <p>Here are my house rules:</p>
+          <p>Natural blackjacks are not automatic wins and pay the normal bet amount</p>
+          <p>Player may double on any starting hand</p>
+          <p>Player may split on starting hands with the same value</p>
           <p>Player may only split once per round</p>
-          <p>Player may not double on split hands</p>
+          <p>Player may not double once they split hands</p>
+          <p>Dealer stands on a soft seventeen(hand worth 17 or 7 with an Ace)</p>
         </div>)
     } else {
       return (
