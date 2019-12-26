eb141f52e6f7439589c469bca03b83dffe997d1f
Kevin Chi <kevinchi118@gmail.com>
2019-12-25 21:18:46 -0500

    adds pointer for first hand on split

--- src/player/index.js
+++ src/player/index.js
@@ -16,7 +16,12 @@ class Player extends React.Component {
     const splitHandTotal = calculateHandTotal(player.splitHand)
     return (
       <div className='player'>
-        <h2>Player {player.handTotal > 0 ? player.handTotal : null} {player.handTotal > 21 ? 'BUST!' : null}</h2>
+        <h2>Player  
+          {player.isSplit ? <br/> : null}
+          {player.isSplit && !player.playingSplitHand ? '👉' : null} 
+          {player.handTotal > 0 ? ' ' + player.handTotal : null} 
+          {player.handTotal > 21 ? 'BUST!' : null}
+        </h2>
         <div>{this.renderHand()}</div>
         <h2>{player.playingSplitHand ? '👉' : null} {splitHandTotal > 0 ? splitHandTotal : null} {splitHandTotal > 21 ? 'BUST!': null}</h2>
         <div>{this.renderSplitHand()}</div>
